e6dc9f075b4c58dfeb74441500f7b290c27176ca
Wenyue Wang <realwwy@cryptape.com>
2018-10-16 09:24:16 +0800

    Add an exchange button

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ window.addEventListener('neuronWebReady', () => {
     window.addMessenger(nervos) // inject new nervos method from neuron-web extension
     setTimeout(() => {
         ReactDOM.render(<App />, document.getElementById('root'));
-    }, 100)
+    }, 100)                     // set timeout to make sure load neurno-web first before render page.
 })
-
-// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
